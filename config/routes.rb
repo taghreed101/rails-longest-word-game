d1a638efecca0280fff186d5d53d2772df6023ba
taghreed <taghreed.alqubati@gmail.com>
2019-07-29 17:23:32 +0800

    rails1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'new', to: 'games#new'
   get 'score', to: 'games#score'
   post 'posts', to: 'games#score' 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
